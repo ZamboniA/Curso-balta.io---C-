a69eafc775978423d57f3fad98ab19c27fbd1ab8
zambonia <andersonzamboni18@gmail.com>
2023-11-30 16:48:02 -0300

    Arrays

--- Fundamentos C# - .netcore 3.1 - LEGADO/Arrays/Program.cs
+++ Fundamentos C# - .netcore 3.1 - LEGADO/Arrays/Program.cs
@@ -6,7 +6,56 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+
+            var meuArray = new int[] { 1, 2, 3, 5, 6 };
+            // meuArray[0] = 12;
+            // int[] testeArray = new int[5];
+
+            // for (var index = 0; index < meuArray.Length; index++)
+            // {
+            //     Console.WriteLine(meuArray[index]);
+            // }
+
+            foreach (var item in meuArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Console.WriteLine(meuArray[0]);
+
+            string[] pessoas = { "anderson", "mayara", "lucy", "gilmara", "priscila", "leticia" };
+
+
+            foreach (var nome in pessoas)
+            {
+                Console.WriteLine(nome);
+            }
+
+            // var funcionarios = new Funcionarios[5];
+            // funcionarios[0] = new Funcionarios() { Id = 01, Name = "Anderson" };
+
+            // foreach (var funcionario in funcionarios)
+            // {
+            //     Console.WriteLine(funcionario.Id);
+            //     Console.WriteLine(funcionario.Name);
+            // }
+
+
+
+            var arr = new int[4];
+            var arrb = new int[4];
+
+            arrb[0] = arr[0];
+            arr.CopyTo(arrb, 0);
+            // arr[0] = 33;
+        }
+
+        public struct Funcionarios
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
     }
 }

fa315a61728468592a309eabe8260c0c80c6f9c8
zambonia <andersonzamboni18@gmail.com>
2023-11-30 12:04:54 -0300

    html editor

---
+++ Fundamentos C# - .netcore 3.1 - LEGADO/HtmlEditor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EditorHtml;
+
+namespace HtmlEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
